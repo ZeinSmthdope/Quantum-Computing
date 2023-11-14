c7cd6cc3b1d022ba52140578c744a6457d1bb182
Zein <106992257+zeinhaidara@users.noreply.github.com>
2023-11-14 18:50:40 -0500

    Add files via upload

---
+++ question4.py
@@ -0,0 +1,51 @@
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+
+
+def grover(n: int):
+    '''
+    This function output Grover's algorithm circuit for a specific target. 
+    Implement the circuit for applying
+        Rotation phase + Inversion around the mean
+    n times, where n is a nonnegative integer.
+    Args: 
+        n: integer of the number of times to apply (rotation + inversion)
+    Return:
+        qcirc: A QuantumCircuit object of your implementation of Grover's algorithm
+    '''
+    qcirc = None
+
+    ############################################################################
+    # Student code begin
+    ############################################################################
+    qcirc = QuantumCircuit(3)
+    qcirc.h([0, 1, 2])
+    qcirc.barrier()
+    
+    for _ in range(n):
+        qcirc.x(0)
+        qcirc.h(2)
+        qcirc.mcx([0, 1], 2)
+        qcirc.x(0)
+        qcirc.h(2)
+        qcirc.barrier()
+        
+        qcirc.h([0, 1, 2])
+        qcirc.x([0, 1, 2])
+        qcirc.barrier()
+    
+        qcirc.h(2)
+        qcirc.mcx([0, 1], 2)
+        qcirc.h(2)
+        qcirc.barrier()
+        
+        qcirc.x([0, 1, 2])
+        qcirc.h([0, 1, 2])
+        qcirc.barrier()
+        qcirc.barrier()
+    ############################################################################
+    # Student code end
+    ############################################################################
+
+    return qcirc
+grover(1) 
+grover(2) 
